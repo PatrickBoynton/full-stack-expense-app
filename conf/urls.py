dab8929857113848765e79f1198dd3b538d1d195
PatrickBoynton <jpboynton3@gmail.com>
2021-06-24 01:30:51 -0400

    Not really sure what has changed.

--- conf/urls.py
+++ conf/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('api/v1/', include('api.urls', namespace='api_v1')),
-                  path('', include('frontend.urls', namespace='frontend'))
+                path('admin/', admin.site.urls),
+                path('api/v1/', include('api.urls', namespace='api_v1')),
+                path('', include('frontend.urls', namespace='frontend'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
