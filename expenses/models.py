c798eccfd9105d0657abb4205d5597c12007b2d7
PatrickBoynton <jpboynton3@gmail.com>
2021-06-27 21:32:02 -0400

    Removed the auto now.

--- expenses/models.py
+++ expenses/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Expenses(models.Model):
     title = models.CharField(max_length=150)
     amount = models.DecimalField(decimal_places=2, max_digits=9)
-    date = models.DateField(auto_now=True)
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.title + ' ' + str(self.date)
