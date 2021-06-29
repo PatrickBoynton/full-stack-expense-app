599c858c7d60c140548023c4cbe0c932c8ab048d
PatrickBoynton <jpboynton3@gmail.com>
2021-06-29 13:37:32 -0400

    Created a unit test for object creation.

--- expenses/tests.py
+++ expenses/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from expenses.models import Expenses
+
 
 # Create your tests here.
+class ExpenseTests(TestCase):
+    def setUp(self):
+        self.expense = Expenses.objects.create(
+            title='123',
+            amount=1235.99,
+            date='2021-03-29 00:15:54'
+        )
+
+    def test_expense_created(self):
+        expense = Expenses.objects.get(id=1)
+        self.assertEqual(expense.title, '123'),
+        self.assertEqual(float(expense.amount), 1235.99),
+        self.assertEqual(f'{expense.date}', '2021-03-29 00:15:54+00:00')
