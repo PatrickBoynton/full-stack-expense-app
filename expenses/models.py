4e902b8f9f809bbad4664e49e466a69e50adf7ab
PatrickBoynton <jpboynton3@gmail.com>
2021-06-29 16:28:12 -0400

    Added defaults to make migrations happy.

--- expenses/models.py
+++ expenses/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from datetime import datetime
 
 
 # Create your models here.
 class Expenses(models.Model):
     title = models.CharField(max_length=150)
-    amount = models.DecimalField(decimal_places=2, max_digits=9)
+    amount = models.DecimalField(decimal_places=2,
+                                 max_digits=9,
+                                 default=0)
     is_recurring = models.BooleanField(default=False)
-    date = models.DateTimeField()
+    date = models.DateTimeField(default=datetime.today())
 
     def __str__(self):
         return self.title + ' ' + str(self.date)
