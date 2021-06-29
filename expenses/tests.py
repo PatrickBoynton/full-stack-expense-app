df5a7d6b95d1f8ab7d501dd1f6696de36e043e8a
PatrickBoynton <jpboynton3@gmail.com>
2021-06-29 15:17:27 -0400

    Added a test for the recurring expense field.

--- expenses/tests.py
+++ expenses/tests.py
@@ -8,6 +8,7 @@ class ExpenseTests(TestCase):
         self.expense = Expenses.objects.create(
             title='123',
             amount=1235.99,
+            is_recurring=False,
             date='2021-03-29 00:15:54'
         )
 
@@ -15,4 +16,5 @@ class ExpenseTests(TestCase):
         expense = Expenses.objects.get(id=1)
         self.assertEqual(expense.title, '123'),
         self.assertEqual(float(expense.amount), 1235.99),
+        self.assertFalse(expense.is_recurring),
         self.assertEqual(f'{expense.date}', '2021-03-29 00:15:54+00:00')
