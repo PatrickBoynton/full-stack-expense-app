7343cc7c2a6600fc21cdf3ff47dbe42e9190f278
PatrickBoynton <jpboynton3@gmail.com>
2021-06-29 16:29:46 -0400

    Added income tests.

--- income/tests.py
+++ income/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Income
+
 
 # Create your tests here.
+class IncomeTests(TestCase):
+    def setUp(self):
+        self.income = Income.objects.create(
+            title='Test title',
+            amount=123,
+            is_recurring=False,
+            date='2021-03-29 00:15:54'
+        )
+
+    def test_income_creation(self):
+        income = Income.objects.get(id=1)
+        self.assertEqual(income.title, 'Test title')
+        self.assertEqual(income.amount, 123)
+        self.assertFalse(income.is_recurring, False)
+        self.assertEqual(f'{income.date}', '2021-03-29 00:15:54+00:00')
