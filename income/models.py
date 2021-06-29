a73412678c0e54a2c021990ff205baf748305398
PatrickBoynton <jpboynton3@gmail.com>
2021-06-29 16:28:30 -0400

    Added fields to create new incomes.

--- income/models.py
+++ income/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 
 # Create your models here.
 class Income(models.Model):
-    pass
+    title = models.CharField(max_length=150,
+                             null=True,
+                             blank=True)
+    amount = models.DecimalField(decimal_places=2,
+                                 max_digits=9,
+                                 default=0)
+    is_recurring = models.BooleanField(default=False)
+    date = models.DateTimeField(default=datetime.today())
+
+    def __str__(self):
+        return self.title + ' ' + str(self.date)
