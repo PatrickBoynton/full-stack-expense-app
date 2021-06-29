3f4eb7e6db446126adefbb867541f5a37bd6240a
PatrickBoynton <jpboynton3@gmail.com>
2021-06-29 15:16:25 -0400

    Added a field for recurring expenses.

--- expenses/models.py
+++ expenses/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Expenses(models.Model):
     title = models.CharField(max_length=150)
     amount = models.DecimalField(decimal_places=2, max_digits=9)
+    is_recurring = models.BooleanField(default=False)
     date = models.DateTimeField()
 
     def __str__(self):
