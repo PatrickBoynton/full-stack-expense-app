f7823415944c491c86cc46b496f4ca29e9b4281c
PatrickBoynton <jpboynton3@gmail.com>
2021-07-01 13:14:40 -0400

    Added the url for the year.

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ExpenseListView
+from .views import ExpenseListView, ExpenseDateListView
 
 app_name = 'expenses'
 
 urlpatterns = [
-    path('', ExpenseListView.as_view())
+    path('', ExpenseListView.as_view()),
+    path('dates/', ExpenseDateListView.as_view())
 ]
