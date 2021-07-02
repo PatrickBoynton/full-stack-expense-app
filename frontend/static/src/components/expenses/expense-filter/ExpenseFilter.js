6a92b99cd96dc33137964d1bfa766d877debf28e
PatrickBoynton <jpboynton3@gmail.com>
2021-07-02 00:31:00 -0400

    Working on making the filter dynamic.

--- frontend/static/src/components/expenses/expense-filter/ExpenseFilter.js
+++ frontend/static/src/components/expenses/expense-filter/ExpenseFilter.js
@@ -5,11 +5,8 @@ const ExpenseFilter = ({onChangeYear}) => {
     return (
         <div className="expenses-filter">
             <div className="expenses-filter__control">
-                <label htmlFor="Filter">Filter by year</label>
+                <label htmlFor="filter">Filter by year</label>
                 <select name="filter" id="filter" onChange={onChangeYear}>
-                    <option value="2022">2022</option>
-                    <option value="2021">2021</option>
-                    <option value="2020">2020</option>
                 </select>
             </div>
         </div>
