562767e2384a3685cbd8f0b637fc65c3870a7bc6
PatrickBoynton <jpboynton3@gmail.com>
2021-07-02 00:57:26 -0400

    Removed the dates url.

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import ExpenseListView, ExpenseDateListView
+from .views import ExpenseListView
 
 app_name = 'expenses'
 
 urlpatterns = [
     path('', ExpenseListView.as_view()),
-    path('dates/', ExpenseDateListView.as_view())
 ]
