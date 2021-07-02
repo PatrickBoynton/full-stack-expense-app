6b5b35117ff1ea21ec995e8a11d7ddb2cbe5bcc4
PatrickBoynton <jpboynton3@gmail.com>
2021-07-02 00:55:09 -0400

    Changed the format of the date_created.

--- expenses/serializers.py
+++ expenses/serializers.py
@@ -1,8 +1,12 @@
+import datetime
+
 from rest_framework import serializers
 from .models import Expenses
 
 
 class ExpensesSerializer(serializers.ModelSerializer):
+    date_created = serializers.DateTimeField(format='%Y-%m-%d %H:%M')
+
     class Meta:
         model = Expenses
         fields = ('id', 'title', 'amount', 'date_created',)
@@ -11,4 +15,4 @@ class ExpensesSerializer(serializers.ModelSerializer):
 class ExpensesDateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expenses
-        fields = ('id', 'date_created', )
+        fields = ('id', 'date_created',)
