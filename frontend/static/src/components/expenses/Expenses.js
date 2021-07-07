41deeafc6888395ec70e935d7ab05f968a0d5439
PatrickBoynton <jpboynton3@gmail.com>
2021-07-07 17:54:59 -0400

    Attempting to fix the filter problem.

--- frontend/static/src/components/expenses/Expenses.js
+++ frontend/static/src/components/expenses/Expenses.js
@@ -6,21 +6,19 @@ import ExpenseItem from './expense-item/ExpenseItem';
 import NoExpense from './no-expense/NoExpense';
 
 const Expenses = ({expenses}) => {
-
     const [year, setYear] = useState();
 
     const changeYear = (event) => {
         setYear(event.target.value);
     };
-
     const filter = expenses
         .filter((expense) => expense.date.getFullYear().toString() === year)
         .map((expense) => <ExpenseItem key={expense.id} year={year} expenses={expense}/>);
 
     return (
         <Card>
-            <ExpenseFilter onChangeYear={changeYear}/>
-            <ExpenseChart expenses={expenses}/>
+            <ExpenseFilter onChangeYear={changeYear} expenses={expenses}/>
+            <ExpenseChart expenses={expenses} filter={filter}/>
             {filter.length === 0 ? <NoExpense/> : filter}
         </Card>
     );
