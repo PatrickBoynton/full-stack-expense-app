3afe8cb4698054019bba7507d79ba3d235eb64be
PatrickBoynton <jpboynton3@gmail.com>
2021-07-07 17:50:58 -0400

    Added a filter to only show dates from the database.

--- frontend/static/src/components/expenses/expense-filter/ExpenseFilter.js
+++ frontend/static/src/components/expenses/expense-filter/ExpenseFilter.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './ExpenseFilter.css';
 
-const ExpenseFilter = ({onChangeYear}) => {
-    return (
-        <div className="expenses-filter">
-            <div className="expenses-filter__control">
-                <label htmlFor="filter">Filter by year</label>
-                <select name="filter" id="filter" onChange={onChangeYear}>
-                </select>
-            </div>
-        </div>
-    );
+const ExpenseFilter = ({ onChangeYear, expenses }) => {
+  const testArray = [];
+
+  expenses.map((expense) => testArray.push(expense.date.getFullYear()));
+
+  const filterTest = testArray.filter((date, index) => testArray.indexOf(date) === index);
+
+  const test = filterTest.map((options, index) => <option key={index}>{options}</option>);
+
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label htmlFor="filter">Filter by year</label>
+        <select name="filter" id="filter" onChange={onChangeYear}>
+          {test}
+        </select>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseFilter;
