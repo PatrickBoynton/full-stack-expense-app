5312b2e8ef58255f5c3e593c0709b873e0eba4de
PatrickBoynton <jpboynton3@gmail.com>
2021-07-13 16:31:57 -0400

    Refactored to use the new hooks.

--- frontend/static/src/components/expense-form/ExpenseForm.js
+++ frontend/static/src/components/expense-form/ExpenseForm.js
@@ -1,27 +1,14 @@
 import React, {useState} from 'react';
 import EditMode from './edit-mode/EditMode';
 import Create from './create/Create';
+import useInput from '../hooks/useInput';
+
 import './ExpenseForm.css';
 
 const ExpenseForm = ({onSubmitExpense}) => {
-    const [inputValue, setInputValue] = useState({title: '', amount: 0, date: new Date()});
     const [editMode, setEditMode] = useState(false);
+    const [inputValue, handleInput] = useInput();
 
-    const handleInput = (event) => {
-        if (event.target.name === 'date') console.log(event.target.value);
-
-        if (event.target.name === 'amount') {
-            setInputValue({
-                ...inputValue,
-                [event.target.name]: +event.target.value,
-            });
-        } else {
-            setInputValue({
-                ...inputValue,
-                [event.target.name]: event.target.value,
-            });
-        }
-    };
 
     const handleSubmit = (event) => {
         event.preventDefault();
